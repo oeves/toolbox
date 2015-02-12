f255ff524561bfbf4e4eaaa20bdb3bb1afd30271
root <post@svenheim.de>
2015-02-12 14:30:56 +0100

    modified:   owncloud/oc-backup.sh

--- owncloud/oc-backup.sh
+++ owncloud/oc-backup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-## owncloud incremental backup script v1
+## owncloud incremental backup script v1.1
 ## requires working PubkeyAuthentication
 ## how to run: ./oc_backup.sh cloud.yourdomain.com
 
 ## known issues:
-## script doesnt backup owncloud data directory when its outside the owncloud installation
+## not tested what happens when remotedatapath is not present
+## should fetch it from the oc config and fetch it depending on it
 
 YESTERDAY=$(date +%u_%A -d "yesterday")
 TODAY=$(date +%u_%A)
 REMOTEHOST=$1
 REMOTEUSER=root
 REMOTEPATH=/var/www/owncloud
+REMOTEDATAPATH=/var/www/owncloud-data
 DESTINATION=/root/oc_backup
 DBPASSWORD=$(cat $DESTINATION/$TODAY/owncloud/config/config.php | grep dbpassword | awk -F\' '{print $4}')
 DBUSER=$(cat $DESTINATION/$TODAY/owncloud/config/config.php | grep dbuser | awk -F\' '{print $4}')
 DBNAME=$(cat $DESTINATION/$TODAY/owncloud/config/config.php | grep dbname | awk -F\' '{print $4}')
 
-rsync -aP --delete --link-dest=$DESTINATION/$YESTERDAY $REMOTEUSER@$REMOTEHOST:$REMOTEPATH $DESTINATION/$TODAY
+rsync --ipv6 -aP --delete --link-dest=$DESTINATION/$YESTERDAY $REMOTEUSER@$REMOTEHOST:$REMOTEPATH :$REMOTEDATAPATH $DESTINATION/$TODAY
 ssh -l $REMOTEUSER $REMOTEHOST "mysqldump -u$DBUSER -p$DBPASSWORD $DBNAME | gzip -3 -c" > $DESTINATION/$TODAY/owncloud.sql.gz
 
 rm $DESTINATION/current.backup
